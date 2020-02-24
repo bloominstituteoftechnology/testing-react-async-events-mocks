16615508f237b1fcf4842da64f6527b35d5ac0ac
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-02-24 10:32:30 +0100

    wip

--- src/components/Display.test.js
+++ src/components/Display.test.js
@@ -2,14 +2,25 @@ import React from 'react'
 import Display from './Display'
 import * as rtl from '@testing-library/react'
 
-describe.skip('Display', () => {
+describe('Display component', () => {
+  // the virtual dom where we mount
+  // componets would get bigger and bigger
+  // every time we mount "Display"
   afterEach(rtl.cleanup)
 
   it('displays the correct type', () => {
-
+    // setup! Mount the thing
+    const wrapper = rtl.render(
+      <Display type='Happiness' data={199} />
+    )
+    // grab an element
+    const theType = wrapper.queryByText(/Happiness/)
+    // run assertions
+    expect(theType).toBeInTheDocument()
+    expect(theType).toBeVisible()
   })
 
-  it('displays the correct data', () => {
+  it.skip('displays the correct data', () => {
 
   })
 })
