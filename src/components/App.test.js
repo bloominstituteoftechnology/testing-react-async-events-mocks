c945389cf4312927ffda37e927cc5a0bf6b69cc9
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-02-24 11:17:17 +0100

    wip

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -4,21 +4,32 @@ import * as rtl from '@testing-library/react'
 
 describe.skip('App', () => {
   afterEach(rtl.cleanup)
+  beforeEach(() => {
+    // whatever needs to happen befere each test
+  })
+  // afterEach()
+  // beforeEach()
 
   it('displays the correct types', () => {
-
+    // it renders Health text
+    // it renders Morale text
+    // QUESTIONABLE TEST ?
   })
 
   it('displays the correct data', () => {
-
+    // it renders 100 for Health
+    // it renders 100 for Health
+    // QUESTIONABLE TEST ?
   })
 
   it('displays the correct button', () => {
-
+    // it render specifically
+    // exercise, eatJunkFood and catchUpWithFriends
+    // QUESTIONABLE TEST ?
   })
 
   it('clicking eat junk food changes health and morale', () => {
-
+    // want to do it?
   })
 
   it('displays the correct name starting with the API data', async () => {
